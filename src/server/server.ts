e39d41945f349eba3d0bce4e6521c100a5e0d863
Gabe <gabrielgrechuk@outlook.com>
2024-09-16 22:49:07 -0300

    #1 - Basic ejs setup

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
 
 app.get('/', (_, res) => {
-  res.send('Hello World!');
+  res.render('home', {
+    title: "Gabe's Square!",
+    header: 'Hello World!',
+  });
 });
 
 export default {
