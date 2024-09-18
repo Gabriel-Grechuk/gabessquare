0eb3b87aed756fbeaed3c4148d4893a70ffb7d46
Gabe <gabrielgrechuk@outlook.com>
2024-09-17 23:02:32 -0300

    #3 - Update view path finding

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import path from 'path';
+import { resolve } from 'path';
 
 const app = express();
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', resolve('./src/views'));
 
 app.get('/', (_, res) => {
   res.render('home', {
